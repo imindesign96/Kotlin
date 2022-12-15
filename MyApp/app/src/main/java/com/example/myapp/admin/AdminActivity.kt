ab8c45c712b88256c7b7bacc8de78440e99fe473
imindesign96 <phanngocvu89@gmail.com>
2022-12-15 18:14:51 +0900

    Created Sales Fragment, Users Fragment

--- MyApp/app/src/main/java/com/example/myapp/admin/AdminActivity.kt
+++ MyApp/app/src/main/java/com/example/myapp/admin/AdminActivity.kt
@@ -1,36 +1,84 @@
 package com.example.myapp.admin
 
 
+import android.app.ProgressDialog.show
+import android.content.ClipData.Item
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
 import com.example.myapp.databinding.ActivityAdminBinding
 
 
 class AdminActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdminBinding
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().apply {
+            replace(com.example.myapp.R.id.frame1, fragment)
+            commit()
+        }
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.cardView1?.setOnClickListener {
-            // If the CardView is already expanded, set its visibility
-            // to gone and change the expand less icon to expand more.
-            if (binding.hiddenView.visibility === View.GONE) {
-                binding.cardView2.visibility = View.GONE
-                binding.hiddenView.visibility = View.VISIBLE
-            }
+       replaceFragment(FragmentInventory())
+       binding.bottomNavigationView.setOnItemSelectedListener {
+           when(it.itemId){
+               com.example.myapp.R.id.navi_smile -> replaceFragment(FragmentInventory())
+               com.example.myapp.R.id.navi_pets -> replaceFragment(FragmentSales())
+               com.example.myapp.R.id.navi_sun -> replaceFragment(FragmentUsers())
+               else -> {
 
-        }
-        //click anywhere searchView
-        binding.searchViewInventory.setOnClickListener(View.OnClickListener {
-            binding.searchViewInventory.setIconified(
-                false
-            )
-        })
+               }
+           }
+           true
+       }
+
+
+
+//        binding.cardView1?.setOnClickListener {
+//            // If the CardView is already expanded, set its visibility
+//            // to gone and change the expand less icon to expand more.
+//            if (binding.hiddenView.visibility === View.GONE) {
+//                binding.cardView2.visibility = View.GONE
+//                binding.hiddenView.visibility = View.VISIBLE
+//            }
+//
+//        }
+//
+//        binding.outlinedButton?.setOnClickListener {
+//            // If the CardView is already expanded, set its visibility
+//            // to gone and change the expand less icon to expand more.
+//            if (binding.hiddenView.visibility === View.VISIBLE) {
+//                binding.cardView2.visibility = View.VISIBLE
+//                binding.hiddenView.visibility = View.GONE
+//            }
+//
+//        }
+//
+//
+//
+//        //click anywhere searchView
+//        binding.searchViewInventory.setOnClickListener(View.OnClickListener {
+//            binding.searchViewInventory.setIconified(
+//                false
+//            )
+//        })
 
 
     }
+
+
+
+
 }
---
+++ MyApp/app/src/main/java/com/example/myapp/admin/FragmentSales.kt
@@ -0,0 +1,20 @@
+package com.example.myapp.admin
+
+import androidx.fragment.app.Fragment
+
+import com.example.myapp.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentInventory.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentSales : Fragment(R.layout.fragment_sales) {
+    
+
+}
