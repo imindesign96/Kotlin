5fbc5eb79065b61bd72d1daae32a7497acd1bff2
thao <thanhthao120799@gmail.com>
2023-01-17 17:10:38 +0700

    Admin/User Role Division

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -6,8 +6,10 @@ import android.util.Log
 import android.view.Menu
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -27,12 +29,10 @@ import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val firebaseConnection: FirebaseConnection by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private lateinit var auth: FirebaseAuth
     private lateinit var signOutBtn: Button
     private lateinit var navController: NavController
-    private lateinit var user : UsersData
-    private lateinit var dbRef : DatabaseReference
 
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -40,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        auth = FirebaseAuth.getInstance()
 
         // Get the navigation host fragment from this Activity
         val navHostFragment = supportFragmentManager
@@ -56,58 +55,12 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
 
-        val email = auth.currentUser?.email
-        val saveEmail = email?.replace(".",",")
-
-        dbRef =
-            saveEmail?.let {
-                FirebaseDatabase.getInstance().getReference("User").child("UsersData").child(
-                    it
-                ).child("status")
-            }!!
-
-        dbRef.get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                when(task.result?.getValue(String::class.java)){
-                    "未支払い" -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_payFragment)
-                    "支払済み" -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_self)
-                    else -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_self)
-                }            } else {
-                // handle error
+        firebaseConnection.user.observe(this) {
+            when (it.status) {
+                "未支払い" -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_payFragment)
+                "支払済み" -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_self)
+                else -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_self)
             }
-        }
-        binding.bottomNavigationViewMain.setOnNavigationItemSelectedListener(){
-            val currentFragment = navHostFragment?.childFragmentManager?.fragments?.get(0)
-            if (currentFragment is HomeFragment) {
-                when(it.itemId){
-                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_self)
-                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_fragmentSimTotal)
-                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_fragmentSales)
-                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_fragmentUsers)
-                }
-            } else if (currentFragment is FragmentSimTotal) {
-                when(it.itemId){
-                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSimTotal_to_homeFragment)
-                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSimTotal_self)
-                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSimTotal_to_fragmentSales)
-                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSimTotal_to_fragmentUsers)
-                }
-            } else if (currentFragment is FragmentSales) {
-                when(it.itemId){
-                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_homeFragment)
-                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_fragmentSimTotal)
-                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_self)
-                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_fragmentUsers)
-                }
-            } else {
-                when(it.itemId){
-                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_homeFragment)
-                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_fragmentSimTotal)
-                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_fragmentSales)
-                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_self)
-                }
-            }
-            true
         }
     }
 
@@ -119,8 +72,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
+        firebaseConnection.user.observe(this) {
+            if (it.role == Users.POTENTIAL_USER) {
+                binding.potentialBottomNav.isVisible = true
+                menuInflater.inflate(R.menu.potential_user_bottomnav, menu)
+            } else {
+                binding.presentBottomNav.isVisible = true
+                menuInflater.inflate(R.menu.present_user_bottomnav, menu)
+            }
+        }
         return true
     }
-
 }
--- MyApp/app/src/main/java/com/example/myapp/admin/AdminActivity.kt
+++ MyApp/app/src/main/java/com/example/myapp/admin/AdminActivity.kt
@@ -1,9 +1,18 @@
 package com.example.myapp.admin
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.myapp.FirebaseConnection
 import com.example.myapp.R
+import com.example.myapp.Users
+import com.example.myapp.admin.home.FragmentHomeAdmin
 import com.example.myapp.admin.home.HomeFragment
 import com.example.myapp.admin.total.FragmentSimTotal
 import com.example.myapp.admin.users.FragmentUsers
@@ -13,44 +22,61 @@ import com.google.firebase.messaging.FirebaseMessaging
 
 class AdminActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private val firebaseConnection: FirebaseConnection by viewModels()
     private lateinit var binding : ActivityAdminBinding
-    private fun replaceFragment(fragment : Fragment){
-        if (onSaveInstanceState(Bundle()) == null){
-            val fragmentManager = supportFragmentManager
-            fragmentManager.beginTransaction().apply {
-                replace(R.id.frame1, fragment)
-                commit()
-            }
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        replaceFragment(FragmentUsers())
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-
-
-       binding.bottomNavigationView.setOnItemSelectedListener {
-           when(it.itemId){
-               R.id.navi_home -> replaceFragment(HomeFragment())
-               R.id.navi_smile -> replaceFragment(FragmentSimTotal())
-               R.id.navi_pets -> replaceFragment(FragmentSales())
-               R.id.navi_sun -> replaceFragment(FragmentUsers())
-               else -> {
-
-               }
-           }
-           true
-       }
-
-
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener() {
+            val currentFragment = navHostFragment?.childFragmentManager?.fragments?.get(0)
+            if (currentFragment is FragmentHomeAdmin) {
+                when (it.itemId) {
+                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentHomeAdmin_self)
+                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentHomeAdmin_to_fragmentInventory)
+                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentHomeAdmin_to_fragmentSales)
+                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentHomeAdmin_to_fragmentUsers)
+                }
+            } else if (currentFragment is FragmentInventory) {
+                when (it.itemId) {
+                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentInventory_to_fragmentHomeAdmin)
+                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentInventory_self)
+                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentInventory_to_fragmentSales)
+                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentInventory_to_fragmentUsers)
+                }
+            } else if (currentFragment is FragmentSales) {
+                when (it.itemId) {
+                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_fragmentHomeAdmin)
+                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_fragmentInventory)
+                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_self)
+                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentSales_to_fragmentUsers)
+                }
+            } else {
+                when (it.itemId) {
+                    R.id.navi_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_fragmentHomeAdmin)
+                    R.id.navi_smile -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_fragmentInventory)
+                    R.id.navi_pets -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_to_fragmentSales)
+                    R.id.navi_sun -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_fragmentUsers_self)
+                }
+            }
+            true
+        }
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
-
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
+        return true
+    }
 }
